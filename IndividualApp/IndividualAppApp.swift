6663e2831de523fbeed03599aba196f23a3e7677
Amabile <nikoamazon88@gmail.com>
2024-12-03 19:02:14 +0100

    Initial Commit

---
+++ IndividualApp/IndividualAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  IndividualAppApp.swift
+//  IndividualApp
+//
+//  Created by Nicolò Amabile on 03/12/24.
+//
+
+import SwiftUI
+
+@main
+struct IndividualAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
